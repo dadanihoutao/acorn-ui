02dbd3a321a1bf34b62c78184ed96366d39e0102
SHI <851051279@qq.com>
2024-09-23 23:33:15 +0800

    feat: :sparkles: test husky

--- packages/utils/install.ts
+++ packages/utils/install.ts
@@ -1,20 +1,25 @@
-import type { App, Plugin } from 'vue'
 import { each } from 'lodash-es'
+
+import type { App, Plugin } from 'vue'
 
 type SFCWithInstall<T> = T & Plugin
 
 export function makeInstaller(components: Plugin[]) {
-  const installer = (app: App) => each(components, (c) => {app.use(c)})
+  const installer = (app: App) =>
+    each(components, (c) => {
+      app.use(c)
+    })
 
   return installer as Plugin
 }
 
 export const withInstall = <T>(component: T) => {
-  (component as SFCWithInstall<T>).install = (app: App) => {
-    const name = (component as any).name
+  ;(component as SFCWithInstall<T>).install = (app: App) => {
+    const { name } = component as any
     if (name) {
       app.component(name, component as Plugin)
     }
   }
+
   return component as SFCWithInstall<T>
 }
